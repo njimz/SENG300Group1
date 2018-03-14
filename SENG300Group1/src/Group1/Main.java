3d9e4913827c93f44598029b2008fbe2a04de4d6
nickjimenez <33402058+nickjimenez@users.noreply.github.com>
2018-03-13 19:36:17 -0600

    Update Main.java
    added one parameter

--- SENG300Group1/src/Group1/Main.java
+++ SENG300Group1/src/Group1/Main.java
@@ -10,21 +10,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		String pathName = "";
 		String type = "";
+		boolean hasPackage = false;
 		
 		try { // check if user gives 2 arguments
 			pathName = args[0];
 			type = args[1];
-			/*
-			Scanner reader = new Scanner(System.in);
-			System.out.println("Enter a path: ");
-			pathName = reader.next();
-			System.out.println("Enter a java type: ");
-			type = reader.next();
-			reader.close();
-			if (type.length()==0) {
-				System.out.println("ERROR: Invalid Java type!");
-				System.exit(0);
-			}*/
 		}
 		//throws out of bounds exception if user does not input 2 arguments
 		catch (java.lang.ArrayIndexOutOfBoundsException e){ 
@@ -42,7 +32,12 @@ public class Main {
 				//System.exit(0);
 			}
 		}
-		ProcessFile fileToProcess = new ProcessFile(pathName, type.toLowerCase());
+		
+		if (type.contains(".")) {
+			hasPackage = true;
+		}
+		
+		ProcessFile fileToProcess = new ProcessFile(pathName, type.toLowerCase(), hasPackage);
 		try {
 			fileToProcess.parseFile();
 		}catch (IOException e){
